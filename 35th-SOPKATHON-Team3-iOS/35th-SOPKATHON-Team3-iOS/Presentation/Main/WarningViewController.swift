45210636d8837d322b62a77411dc887da999952a
Yurim Kim <yurim830@gmail.com>
2024-11-24 01:22:48 +0900

    [Add] #7 - WarningVC 추가

---
+++ 35th-SOPKATHON-Team3-iOS/35th-SOPKATHON-Team3-iOS/Presentation/Main/WarningViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WarningViewController.swift
+//  35th-SOPKATHON-Team3-iOS
+//
+//  Created by 김유림 on 11/24/24.
+//
+
+import UIKit
+
+class WarningViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
