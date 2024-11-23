f5ae0e11b62379107bbe2c713657035c2b87aa01
0gonge <Challengef0802@gmail.com>
2024-11-24 00:04:20 +0900

    [Feat] #10 - Button 구현

--- 35th-SOPKATHON-Team3-iOS/35th-SOPKATHON-Team3-iOS/Presentation/Result/ViewController/ExcuseViewController.swift
+++ 35th-SOPKATHON-Team3-iOS/35th-SOPKATHON-Team3-iOS/Presentation/Result/ViewController/ExcuseViewController.swift
@@ -12,6 +12,14 @@ import Then
 
 class ExcuseViewController: UIViewController {
     
+    private let keepGoingButton = UIButton().then {
+        $0.titleLabel?.font = .head(.h4Semibold)
+        $0.setTitle("그냥 마시기", for: .normal)
+        $0.setTitleColor(.primary200, for: .normal)
+        $0.layer.cornerRadius = 29
+        $0.backgroundColor = .primary500
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setStyle()
@@ -20,15 +28,25 @@ class ExcuseViewController: UIViewController {
     }
     
     private func setStyle() {
-        
+        self.view.backgroundColor = .primary200
     }
     
     private func setUI() {
-        
+        self.view.addSubviews(keepGoingButton)
     }
     
     private func setLayout() {
-        
+        keepGoingButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(50)
+            $0.leading.equalTo(31)
+            $0.width.equalTo(314)
+            $0.height.equalTo(56)
+        }
     }
         
+}
+
+#Preview {
+    ExcuseViewController()
 }
