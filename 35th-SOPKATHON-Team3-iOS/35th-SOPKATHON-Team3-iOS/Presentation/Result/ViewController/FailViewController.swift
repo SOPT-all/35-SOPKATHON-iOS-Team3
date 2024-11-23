54d7d3ed01549d4c25c95d5e17a15d9aeb868d1b
0gonge <Challengef0802@gmail.com>
2024-11-24 02:30:06 +0900

    [Feat] #18 - 퀴즈 결과 뷰 작업

---
+++ 35th-SOPKATHON-Team3-iOS/35th-SOPKATHON-Team3-iOS/Presentation/Result/ViewController/FailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FailViewController.swift
+//  35th-SOPKATHON-Team3-iOS
+//
+//  Created by 송여경 on 11/23/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class FailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setStyle()
+        setUI()
+        setLayout()
+    }
+    
+    private func setStyle() {
+        
+    }
+    
+    private func setUI() {
+
+    }
+    
+    private func setLayout() {
+        
+    }
+    
+}
